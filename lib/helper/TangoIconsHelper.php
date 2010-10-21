226c2cf3b27a6c5eae6575d2dcc75d6556ebc44c
tomi <tomi@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-10-21 08:50:15 -0500

    Change the sequence of parameters
    git-svn-id: http://svn.symfony-project.com/plugins/sfTangoIconsPlugin/trunk@31211 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/helper/TangoIconsHelper.php
+++ lib/helper/TangoIconsHelper.php
@@ -2,11 +2,11 @@
 
 /**
  * Return a path to the icon.
- * @param integer $size Icon size
  * @param string $context Context for the icon
  * @param string $name Name of the icon
+ * @param integer $size Icon size
  * @return string Path to the icon
  */
-function ti_get_image_path($size, $context, $name) {
+function ti_get_image_path($context, $name, $size) {
 	return sfContext::getInstance()->getRequest()->getRelativeUrlRoot()."/sfTangoIconsPlugin/images/{$size}x{$size}/{$context}/{$name}.png";
 }
