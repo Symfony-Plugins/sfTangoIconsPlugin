83e875ab8e214b40af0bfcc82df541ce632e19f7
tomi <tomi@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-10-21 08:45:23 -0500

    Model class of icon sizes
    git-svn-id: http://svn.symfony-project.com/plugins/sfTangoIconsPlugin/trunk@31210 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/model/TangoIconsSize.class.php
@@ -0,0 +1,17 @@
+<?php
+class TangoIconsSize {
+	/**
+	 * Small icons
+	 */
+	const SMALL = 16;
+
+	/**
+	 * Medium icons
+	 */
+	const MEDIUM = 22;
+
+	/**
+	 * Large icons
+	 */
+	const LARGE = 32;
+}
