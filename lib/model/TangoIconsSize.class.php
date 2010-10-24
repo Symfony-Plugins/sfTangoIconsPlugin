b05f89976a9d64b0a34c1380c4bae72c4a50141b
tomi <tomi@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-10-24 01:50:25 -0500

    function to normalize icon size
    git-svn-id: http://svn.symfony-project.com/plugins/sfTangoIconsPlugin/trunk@31234 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/model/TangoIconsSize.class.php
+++ lib/model/TangoIconsSize.class.php
@@ -1,20 +1,76 @@
 <?php
-class TangoIconsSize {
-	/**
-	 * At 16×16 pixels, the "Extra Small" size is used in places such as
-         * lists (file dialog, message list in an e-mail client, etc.) and menus.
-         *
-	 */
-	const EXTRA_SMALL = 16;
+class TangoIconsSize
+{
+  const
+    /**
+    * At 16×16 pixels, the "Extra Small" size is used in places such as
+    * lists (file dialog, message list in an e-mail client, etc.) and menus.
+    */
+    EXTRA_SMALL = 16,
 
-	/**
-         * "Small" is the common size for application toolbar icons. Its bitmap
-         * size is 22×22 pixels.
-	 */
-	const SMALL = 22;
+    /**
+    * "Small" is the common size for application toolbar icons. Its bitmap
+    * size is 22×22 pixels.
+    */
+    SMALL = 22,
 
-	/**
-	 * A very common size of 32x32px is used on some menus (slab).
-	 */
-	const MEDIUM = 32;
+    /**
+    * A very common size of 32x32px is used on some menus (slab).
+    */
+    MEDIUM = 32;
+
+  /**
+   * Normalize the size of the icons.
+   * @param string $size Icon size
+   *  <p>Proper values are:
+   *  <ul>
+   *   <li>For extra small icons:
+   *    <ul>
+   *     <li>16
+   *     <li>16px
+   *     <li>extra-small
+   *     <li>extra small
+   *     <li>x-small
+   *    </ul>
+   *   <li>For small icons:
+   *    <ul>
+   *     <li>22
+   *     <li>22px
+   *     <li>small
+   *    </ul>
+   *   <li>For medium icons:
+   *    <ul>
+   *     <li>32
+   *     <li>32px
+   *     <li>medium
+   *    </ul>
+   *  </ul>
+   *  </p>
+   * @return int Icon size in pixels
+   */
+  public static function normalize($size)
+  {
+    $size = strtolower(trim((string)$size));
+    
+    switch($size)
+    {
+      case '16':
+      case '16px':
+      case 'extra-small':
+      case 'extra small':
+      case 'x-small':
+        return 16;
+      case '22':
+      case '22px':
+      case 'small':
+        return 22;
+      case '32':
+      case '32px':
+      case 'medium':
+        return 32;
+      default:
+        //TODO: log the bad icon size
+        return 32;
+    }
+  }
 }
