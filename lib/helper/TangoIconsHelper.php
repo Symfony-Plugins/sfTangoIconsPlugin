d42183ddea26df4e153568d8b2a8753dca3b7160
tomi <tomi@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-10-24 15:54:46 -0500

    rename path to uri; add 2 new specific functions
    git-svn-id: http://svn.symfony-project.com/plugins/sfTangoIconsPlugin/trunk@31239 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/helper/TangoIconsHelper.php
+++ lib/helper/TangoIconsHelper.php
@@ -1,16 +1,36 @@
 <?php
 
 /**
- * Return a path to the icon.
+ * Return a URI to the icon.
  * @param string $context Context for the icon
  * @param string $name Name of the icon
  * @param integer $size Icon size
- * @return string Path to the icon
+ * @return string URI to the icon
  */
-function ti_get_image_path($context, $name, $size=null) {
+function ti_get_image_uri($context, $name, $size=null) {
   $size = $size === null ? sfConfig::get('app_tango_icons_size', TangoIconsSize::EXTRA_SMALL) : $size;
   $size = TangoIconsSize::normalize($size);
   return sprintf('%s/sfTangoIconsPlugin/images/%dx%d/%s/%s.png',
     sfContext::getInstance()->getRequest()->getRelativeUrlRoot(),
     $size, $size, $context, $name);
+}
+
+/**
+ * Return a URI to the action icon.
+ * @param string $name Name of the action icon
+ * @param integer $size Icon size
+ * @return string URI to the action icon
+ */
+function ti_get_action_icon_uri($name, $size=null) {
+    return ti_get_image_path(TangoIconsAction::getContext(), $name, $size);
+}
+
+/**
+ * Return a URI to the application icon.
+ * @param string $name Name of the application icon
+ * @param integer $size Icon size
+ * @return string URI to the application icon
+ */
+function ti_get_application_icon_uri($name, $size=null) {
+    return ti_get_image_path(TangoIconsApplication::getContext(), $name, $size);
 }
